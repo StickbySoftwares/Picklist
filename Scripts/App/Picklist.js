cb2eabcf8c17aad9994f46da4a0983e0aece698e
Sanket Joshi <sanketjoshi_2005@rediffmail.com>
2014-02-12 22:32:21 +0530

    Structured javascript
    Self Invoking Functions Created and called the functions from Html with
    new Names

--- Scripts/App/Picklist.js
+++ Scripts/App/Picklist.js
@@ -1,17 +1,25 @@
-    function getPickList() {
-        var data = DataService.getData();
+var PickList=(function(){    
+	
+	 var bindData=function (obj) {
+        $('#searchID').val(($(obj).find('span').text()));
+        $('#searchDescription').val($(obj).parent().parent().find('span[class*="SrNo"]').text());
+
+    };
+	var getPickList=function () {
+       
+        var data = [{ ID: 1, SrNo: 123 }, { ID: 2, SrNo: 456 }, { ID: 3, SrNo: 789 }, { ID: 4, SrNo: 91011 }, { ID: 5, SrNo: 121314 }];
         $('#picklist').html('');
         for (i = 0; i < data.length ; i++) {
             var tr = '<tr class="ID" > ';
-            tr += '<td><a data-dismiss="modal" href="#" onclick="javascript: appendData(this); "><span class="ID">' + data[i].ID + '</span></a></td>';
+            tr += '<td><a data-dismiss="modal" href="#" onclick="javascript: PickList.appendData(this); "><span class="ID">' + data[i].ID + '</span></a></td>';
             tr += '<td><span class="SrNo">' + data[i].SrNo + '</span></td>';
             tr += '</tr>';
             $('#picklist').append(tr);
             $('divGrid').show();
         }
     }
-    function appendData(obj) {
-        $('#searchID').val(($(obj).find('span').text()));
-        $('#searchDescription').val($(obj).parent().parent().find('span[class*="SrNo"]').text());
-
-    }
+	return{
+	showPickList:getPickList,
+	appendData:bindData
+	}
+   }());
